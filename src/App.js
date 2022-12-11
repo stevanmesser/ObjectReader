e080cc7a4a6ccb7d3361eeac09a6e4b08ca5239a
stevanmesser <stevanmesser@gmail.com>
2022-12-11 09:16:46 -0300

    fix unused vars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-// Import dependencies
-import React, { useRef, useState, useEffect } from "react";
+/* eslint-disable no-unused-vars */
+import React, { useRef, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
-import "./App.css";
+
 import { drawRect } from "./utilities";
+import "./App.css";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-
-  // Main function
-  const runCoco = async () => {
-    const net = await cocossd.load();
-    console.log("Handpose model loaded.");
-    //  Loop and detect hands
-    setInterval(() => {
-      detect(net);
-    }, 10);
-  };
 
   const detect = async (net) => {
     // Check data is available
@@ -49,7 +40,19 @@ function App() {
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(()=>{
+    // Main function
+    const runCoco = async () => {
+      const net = await cocossd.load();
+      console.log("Handpose model loaded.");
+      //  Loop and detect hands
+      setInterval(() => {
+        detect(net);
+      }, 10);
+    };
+
+    runCoco()
+  },[]);
 
   return (
     <div className="App">
